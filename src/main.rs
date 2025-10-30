da8a515169a49506be8f54d37145eb1970d1b62d
Barbatruc <6886536+barbatrucs@users.noreply.github.com>
2025-10-30 16:53:37 +0100

    Handling  erros as best as possible with current knowledge

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 use std::io;
 
 fn main() {
+    let mut unit = String::new();
+    let mut temperature = String::new();
+
     println!("--- Welcome to the rusty Temperature Converter program! --- \n");
 
     // Unit input
     println!("Would you like to convert from Fahrenheit (F) or from Celsius(C)?");
 
-    let mut unit = String::new();
-
     io::stdin()
         .read_line(&mut unit)
         .expect("Failed to read line");
 
-    let unit = match unit.trim() {
-        "C" => "C",
-        "F" => "F",
-        _ => "please enter a valid unit, either C for Celsius or F for Fahrenheit",
-    };
+    // shadowing previous value
+    let unit = unit.trim();
 
     // Temperature input
-    println!("please enter a temperature to be converted (expected format: 00.0)");
-
-    let mut temperature = String::new();
+    println!("please enter a temperature to be converted");
 
     io::stdin()
         .read_line(&mut temperature)
         .expect("Failed to read line");
 
-    //Shadowing the previous value of temperature
-    let temperature: f32 = temperature.trim().parse().expect("enter a float please");
-
-    println!("You entered: {temperature}°C");
+    // Shadowing the previous value of temperature
+    let temperature: f32 = match temperature.trim().parse::<f32>() {
+        Ok(num) => num,
+        Err(_) => panic!("please provide a number"), // how to handle this properly ?
+    };
 
     if unit == "C" {
-        println!("You choosed to convert from Celsius.");
+        println!("You choosed to convert {temperature}°C to Fahrenheit.");
         let temperature = (temperature * 9.0 / 5.0) + 32.0;
         println!("result of the conversion: {temperature}°F");
     } else {
-        println!("You choosed to convert from Fahrenheit.");
+        println!("You choosed to convert {temperature}°F to Celsius.");
         let temperature = (temperature - 32.0) * 5.0 / 9.0;
         println!("result of the conversion: {temperature}°C");
     }
